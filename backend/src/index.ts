c9c69e61c8a9c81fcb1d7d5320ea4be66e0de8d1
Mohitjha0806 <mohitjha95844@gmail.comm>
2024-03-18 23:39:47 +0530

    backend setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
-import "dotevn/config";
+import "dotenv/config";
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response) => {
+  res.json({ massage: "Hello !" });
+});
+
+app.listen(7000, () => {
+  console.log("Server started on localhost: 7000");
+});
