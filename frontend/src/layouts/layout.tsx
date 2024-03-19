12fd7e7b70079dcb325696d2815930e1b8bc70ea
Mohitjha0806 <mohitjha95844@gmail.comm>
2024-03-20 02:13:12 +0530

    57 min layout and header done

---
+++ frontend/src/layouts/layout.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/Header";
+
+type props = {
+  children: React.ReactNode;
+};
+
+const Layout = ({ children }: props) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+    </div>
+  );
+};
+
+export default Layout;
