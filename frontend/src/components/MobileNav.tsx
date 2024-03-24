4d06a45382ae7dad0518fee94db8732c3316f164
Mohit Jha <115363079+Mohitjha0806@users.noreply.github.com>
2024-03-24 14:22:32 +0530

    1:04 hr Mobile Nav Part ERROR

---
+++ frontend/src/components/MobileNav.tsx
@@ -0,0 +1,26 @@
+import { Menu } from "lucide-react";
+import { Sheet, SheetContent, SheetTitle, SheetTrigger } from "./ui/sheet";
+import { Menu } from "lucide-react";
+import { Separator } from "./ui/separator";
+
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent>
+        <SheetTitle>
+          <span>Welcome to GoFood.com !</span>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+        <button className="flex-1 font-bold bg-orange-500">Log In</button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
